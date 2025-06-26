89b8fb2659997a19b2417af54bdc7c17c13db27b
Andrea Stanaj <stanajandrea563@gmail.com>
2025-06-26 12:56:57 +0200

    Updated Axios baseURL to Render backend

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,10 +1,11 @@
+// axiosClient.js
 import axios from "axios"
 
-const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+const instance = axios.create({
+  baseURL: "https://hospital-backend-r5bt.onrender.com", 
   headers: {
     "Content-Type": "application/json",
   },
 })
 
-export default axiosClient
+export default instance
